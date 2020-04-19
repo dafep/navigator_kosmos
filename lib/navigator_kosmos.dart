ae7fc7465a067b50765f337513c05d78ee851f36
Larsanov <ismail.larsanov@epitech.eu>
2020-04-19 18:27:04 +0200

    add router and navigateTo

---
+++ lib/navigator_kosmos.dart
@@ -0,0 +1,38 @@
+library navigator_kosmos;
+
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+
+class NavigatorKosmos {
+  navigateTo({context, String route}) {
+    Navigator.pushNamed(context, route);
+  }
+
+  initialRoute({RouteSettings settings, Map<String, dynamic> map}) {
+    return generateRoute(
+      settings: settings,
+      map: map,
+    );
+  }
+}
+
+generateRoute({RouteSettings settings, Map<String, dynamic> map}) {
+  if (map.isNotEmpty) {
+    var entryList = map.entries.toList();
+
+    for (int i = 0; i <= map.length - 1; i += 1) {
+      if (entryList[i].key == settings.name) {
+        Widget child = entryList[i].value;
+        return _getPageRoute(
+          settings: settings,
+          child: child,
+        );
+      }
+    }
+  } else
+    return (null);
+}
+
+PageRoute _getPageRoute({Widget child, RouteSettings settings}) {
+  return PageTransition(type: PageTransitionType.fade, settings: settings, child: child);
+}
